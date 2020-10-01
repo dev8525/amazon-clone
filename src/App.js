35a37862f9538cbfb697c9bd10bc758496497273
dev8525 <kalashmali39@gmail.com>
2020-10-01 21:36:23 +0530

    setup project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => (
+    <div className='app'>
+        <h1>Amazon clone</h1>
+
+        {/* Header */}
+
+        {/* Homepage */}
+            {/* Homepage components */}
+
+        {/* Footer */}
+    </div>
+);
+
+export default App;
